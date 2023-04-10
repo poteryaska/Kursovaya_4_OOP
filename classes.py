a3b6bfcd5ee8f0bfea636ac3125db2d9ced683b3
evgeniia lemanova <jane87.05@mail.ru>
2023-04-10 16:38:34 +0300

    add connect_api

--- classes.py
+++ classes.py
@@ -1,4 +1,29 @@
 from abc import ABC, abstractmethod
+import requests
+import json
+
+url = "https://api.hh.ru/vacancies"
+params = {
+    "text": "python",
+    "per_page": 100,
+    "salary": 50000
+}
+
+response = requests.get(url, params=params)
+
+if response.status_code == 200:
+    vacancies = response.json()["items"]
+    with open("vacancies.json", "w", encoding='utf-8') as write_file:
+        json.dump(vacancies, write_file, indent=4, ensure_ascii=False)
+    for vacancy in vacancies:
+        print(vacancy["name"], vacancy["url"])
+else:
+    print("Error:", response.status_code)
+
+
+
+
+
 
 class Job(ABC):
     @abstractmethod
@@ -8,3 +33,9 @@ class Job(ABC):
     @abstractmethod
     def get_vacancies(self):
         pass
+
+class HeadHunter(Job):
+    pass
+
+class SuperJob(Job):
+    pass
