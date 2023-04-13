3930273886b02f7c5b20d83dc0821aa5f1aab780
evgeniia lemanova <jane87.05@mail.ru>
2023-04-13 22:31:25 +0300

    add Vacancy

--- src/classes.py
+++ src/classes.py
@@ -41,7 +41,6 @@ class HeadHunterApi(Job):
                                          "salary_to": (str(salary_to)).lower(),
                                          "town": (vacancy["area"]["name"]).lower(),
                                          })
-
             with open("vacancies.json", "w", encoding='utf-8') as write_file:
                 json.dump(self.__vacancies, write_file, indent=4, ensure_ascii=False)
                 for vacancy in self.__vacancies:
@@ -71,7 +70,6 @@ class SuperJob(Job):
                                          "salary_to": (str(vacancy["payment_to"])).lower(),
                                          "town": (vacancy["town"]["title"]).lower(),
                                          })
-
             with open("vacancies_SJ.json", "w", encoding='utf-8') as write_file:
                 json.dump(self.__vacancies, write_file, indent=4, ensure_ascii=False)
                 for vacancy in self.__vacancies:
@@ -79,18 +77,54 @@ class SuperJob(Job):
         else:
             print("Error:", response.status_code)
 
-v = HeadHunterApi()
-v.get_vacancies('репетитор')
-print('--------------------------------')
-a = SuperJob()
-a.get_vacancies('репетитор')
+# v = HeadHunterApi()
+# v.get_vacancies('репетитор')
+# print('--------------------------------')
+# a = SuperJob()
+# a.get_vacancies('репетитор')
 
 class Vacancy:
-    def __init__(self, name_vacancy, url_vacancy, town, salary_from=None, salary_to=None):
+    def __init__(self, name_vacancy, url_vacancy, town, salary_from:str=None, salary_to=None):
         self.__name_vacancy = name_vacancy
         self.__url_vacancy = url_vacancy
         self.__salary_from = salary_from
         self.__salary_to = salary_to
         self.__town = town
+    def __str__(self):
+        return f'{self.__name_vacancy}, {self.__url_vacancy}, {self.__town}, {self.__salary_from}, {self.__salary_to}'
+    def __gt__(self, other):
+        if self.__salary_from and other.__salary_from is not None:
+            if int(self.__salary_from) > int(other.__salary_from):
+                return f'В этой вакансии: {self.__name_vacancy} БОЛЬШЕ мin зарплата, чем тут: {other.__name_vacancy}'
+            else:
+                return f'В этой вакансии: {self.__name_vacancy} МЕНЬШЕ мin зарплата, чем тут: {other.__name_vacancy}'
+        else:
+            raise ValueError('Отсутствуют данные о зарплате')
+    def __lt__(self, other):
+        if int(self.__salary_from) < int(other.__salary_from):
+            return f'В этой вакансии: {self.__name_vacancy} МЕНЬШЕ мin зарплата, чем тут: {other.__name_vacancy}'
+
+vacancy1 = Vacancy('учитель', 'https://hh.ru/applicant/vacancy_response?vacancyid=78853115', 'москва')
+vacancy2 = Vacancy('учитель', 'https://hh.ru/applicant/vacancy_response?vacancyid=78853115', 'москва')
+print(vacancy1)
+print(vacancy1 > vacancy2)
+
+class Saver(ABC):
+    @abstractmethod
+    def add_vacancy(self, vacancy):
+        pass
+
+    @abstractmethod
+    def get_vacancies_by_salary(self):
+        pass
+
+    @abstractmethod
+    def delete_vacancy(self):
+        pass
+
+
+class JSONSaver(Saver):
+    def add_vacancy(self, vacancy):
+        pass
 
 
